f6d3160014ff94feac7664a352882a0db52dd42b
Brian Guadalupe <free2sendmail@gmail.com>
2013-05-14 13:03:42 +0800

    Needs command-line arguments

--- day_of_week.py
+++ day_of_week.py
@@ -6,42 +6,49 @@
 # m = month (Jan=1 Feb=2...)
 # d = date
 # y = year
+#
 # It also supports dates from the Julian calendar on or
 # before October 4, 1582 and dates from the Gregorian
 # calendar on or after October 15, 1582.
+# 
+# For example:
+# python day_of_week.py 3 14 1999
 ########################################################
 
+import sys
 from math import trunc
 
-while True:
-    month = input("month? ")
-    day = input("day? ")
-    year = input("year? ")
+month = sys.argv[1]
+day = sys.argv[2]
+year = sys.argv[3]
 
-    def getDayOfWeek(month, day, year):
-        a = trunc((14 - month) / 12)
-        y = year - a
-        m = month + 12 * a - 2
-        if year <= 1582 and month <= 10 and day <= 4:
-            d = trunc((5 + day + y + (y/4) + (31*m/12)) % 7)
-        else:
-            d = trunc((day + y + (y/4) - (y/100) + (y/400) + (31*m/12)) % 7)
-        return d
+d = 0
 
-    def nameDayOfWeek(num):
-        if num == 0:
-            print "It is a Sunday."
-        elif num == 1:
-            print "It is a Monday."
-        elif num == 2:
-            print "It is a Tuesday."
-        elif num == 3:
-            print "It is a Wednesday."
-        elif num == 4:
-            print "It is a Thursday."
-        elif num == 5:
-            print "It is a Friday."
-        else:
-            print "It is a Saturday."
+def getDayOfWeek(month, day, year):
+    a = trunc((14 - month) / 12)
+    y = year - a
+    m = month + 12 * a - 2
+    if year <= 1582 and month <= 10 and day <= 4:
+        d = trunc((5 + day + y + (y/4) + (31*m/12)) % 7)
+    else:
+        d = trunc((day + y + (y/4) - (y/100) + (y/400) + (31*m/12)) % 7)
+    return d
 
-    print nameDayOfWeek(getDayOfWeek(month, day, year))
+
+if d == 0:
+    print "It is a Sunday."
+elif d == 1:
+    print "It is a Monday."
+elif d == 2:
+    print "It is a Tuesday."
+elif d == 3:
+    print "It is a Wednesday."
+elif d == 4:
+    print "It is a Thursday."
+elif d == 5:
+    print "It is a Friday."
+else:
+    print "It is a Saturday."
+
+
+
--- quantnums.py
+++ quantnums.py
@@ -9,9 +9,13 @@
 #  l
 # m  = spin quantum number (-1/2=paired 1/2=unpaired)
 #  s
+#
+# For example:
+# python quantnums.py 3 1 0 +1/2
 ########################################################
 
 from __future__ import division
+import sys
  
 arrows = 0
 atnum = {'Ru': 44, 'Re': 75, 'Rf': 104, 'Rg': 111, 'Ra': 88, 'Rb': 37, 'Rn': 86, 'Rh': 45, 'Be': 4, 'Ba': 56, 'Bh': 107, 'Bi': 83, 'Bk': 97, 'Br': 35, 'H': 1, 'P': 15, 'Os': 76, 'Es': 99, 'Hg': 80, 'Ge': 32, 'Gd': 64, 'Ga': 31, 'Pr': 59, 'Pt': 78, 'Pu': 94, 'C': 6, 'Pb': 82, 'Pa': 91, 'Pd': 46, 'Cd': 48, 'Po': 84, 'Pm': 61, 'Hs': 108, 'Uup': 115, 'Uus': 117, 'Ho': 67, 'Hf': 72, 'K': 19, 'He': 2, 'Md': 101, 'Mg': 12, 'Mo': 42, 'Mn': 25, 'O': 8, 'Mt': 109, 'S': 16, 'W': 74, 'Zn': 30, 'Eu': 63, 'Zr': 40, 'Er': 68, 'Ni': 28, 'No': 102, 'Na': 11, 'Nb': 41, 'Nd': 60, 'Ne': 10, 'Np': 93, 'Fr': 87, 'Fe': 26, 'Fl': 114, 'Fm': 100, 'B': 5, 'F': 9, 'Sr': 38, 'N': 7, 'Kr': 36, 'Si': 14, 'Sn': 50, 'Sm': 62, 'V': 23, 'Sc': 21, 'Sb': 51, 'Sg': 106, 'Se': 34, 'Co': 27, 'Cn': 112, 'Cm': 96, 'Cl': 17, 'Ca': 20, 'Cf': 98, 'Ce': 58, 'Xe': 54, 'Lu': 71, 'Cs': 55, 'Cr': 24, 'Cu': 29, 'La': 57, 'Li': 3, 'Lv': 116, 'Tl': 81, 'Tm': 69, 'Lr': 103, 'Th': 90, 'Ti': 22, 'Te': 52, 'Tb': 65, 'Tc': 43, 'Ta': 73, 'Yb': 70, 'Db': 105, 'Dy': 66, 'Ds': 110, 'I': 53, 'U': 92, 'Y': 39, 'Ac': 89, 'Ag': 47, 'Uut': 113, 'Ir': 77, 'Am': 95, 'Al': 13, 'As': 33, 'Ar': 18, 'Au': 79, 'At': 85, 'In': 49, 'Uuo': 118}
@@ -23,63 +27,61 @@ tuples = [('H', '1s1'), ('He', '1s2'), ('Li', '2s1'), ('Be', 's2'), ('B', 'p1'),
 symdict = {'6s1': 'Cs', '6s2': 'Ba', '3p1': 'Al', '3p2': 'Si', '3p3': 'P', '3p4': 'S', '3p5': 'Cl', '3p6': 'Ar', '2s2': 'Be', '6p5': 'At', '6p4': 'Po', '6p6': 'Rn', '6p1': 'Tl', '6p3': 'Bi', '6p2': 'Pb', '3s1': 'Na', '3s2': 'Mg', '4f9': 'Dy', '4f8': 'Tb', '4f5': 'Sm', '4f4': 'Pm', '4f7': 'Gd', '4f6': 'Eu', '4f1': 'Ce', '2s1': 'Li', '4f3': 'Nd', '4f2': 'Pr', '6d7': 'Mt', '7s1': 'Fr', '7s2': 'Ra', '5p6': 'Xe', '5p4': 'Te', '5p5': 'I', '5p2': 'Sn', '5p3': 'Sb', '5p1': 'In', '2p1': 'B', '2p3': 'N', '2p2': 'C', '2p5': 'F', '2p4': 'O', '2p6': 'Ne', '5f8': 'Bk', '5f9': 'Cf', '5f1': 'Th', '5f2': 'Pa', '5f3': 'U', '5f4': 'Np', '5f5': 'Pu', '5f6': 'Am', '5f7': 'Cm', '4f11': 'Er', '4f10': 'Ho', '4f13': 'Yb', '4f12': 'Tm', '4f14': 'Lu', '4p6': 'Kr', '4p5': 'Br', '4p4': 'Se', '4p3': 'As', '4p2': 'Ge', '4p1': 'Ga', '6d10': 'Cn', '5f12': 'Md', '5f13': 'No', '5f10': 'Es', '5f11': 'Fm', '5f14': 'Lr', '5d2': 'Hf', '3d5': 'Mn', '3d6': 'Fe', '5d1': 'La', '5d6': 'Os', '5d7': 'Ir', '5d4': 'W', '5d5': 'Re', '5d8': 'Pt', '5d9': 'Au', '3d8': 'Ni', '3d9': 'Cu', '1s2': 'He', '1s1': 'H', '6d1': 'Ac', '6d3': 'Db', '6d2': 'Rf', '6d5': 'Bh', '6d4': 'Sg', '3d10': 'Zn', '6d6': 'Hs', '6d9': 'Rg', '6d8': 'Ds', '3d4': 'Cr', '5d3': 'Ta', '3d7': 'Co', '3d1': 'Sc', '3d2': 'Ti', '3d3': 'V', '5s2': 'Sr', '5s1': 'Rb', '4s2': 'Ca', '4s1': 'K', '7p4': 'Lv', '7p5': 'Uus', '7p6': 'Uuo', '7p1': 'Uut', '7p2': 'Fl', '7p3': 'Uup', '4d9': 'Ag', '4d8': 'Pd', '4d3': 'Nb', '4d2': 'Zr', '4d1': 'Y', '4d7': 'Rh', '4d6': 'Ru', '4d5': 'Tc', '4d4': 'Mo', '5d10': 'Hg', '4d10': 'Cd'}
 row18 = [('He', 2), ('Ne', 10), ('Ar', 18), ('Kr', 36), ('Xe', 54), ('Rn', 86)]
 
-while True:
- 
-        print "Enter quantum numbers for n, l, m , m  in order"
-        print "                                 l   s \n"
-        n = input("What is the principal quantum number? ")
-        l = input("What is the orbital quantum number? (s=0 p=1 d=2 f=3) ")
-        m = input("What is the magnetic quantum number? ")
-        s = input("What is the spin quantum number? (-1/2=paired +1/2=unpaired) ")
 
-        if l == 0:
-           spdf = "s"
-        elif l == 1:
-           spdf = "p"
-        elif l == 2:
-           spdf = "d"
-        elif l == 3:
-           spdf = "f"
-        else:
-           print "Argument 'l' is invalid"
+n = int(sys.argv[1])
+l = int(sys.argv[2])
+m = int(sys.argv[3])
+s = sys.argv[4]
+
+if l == 0:
+   spdf = "s"
+elif l == 1:
+   spdf = "p"
+elif l == 2:
+   spdf = "d"
+elif l == 3:
+   spdf = "f"
+else:
+   print "Argument 'l' is invalid"
 
 
-        if l == 0:
-           if s == 1 / 2:
-              arrows = 1
-           else:
-              arrows = 2
-        elif l == 1:
-           if s == 1 / 2:
-              arrows = m + 2
-           else:
-              arrows = m + 5
-        elif l == 2:
-           if s == 1 / 2:
-              arrows = m + 3
-           else:
-              arrows = m + 8
-        elif l == 3:
-           if s == 1 / 2:
-              arrows = m + 4
-           else:
-              arrows = m + 11
-        else:
-           print "Arguments 'l' & 's' are invalid"  
+if l == 0:
+   if s == 1 / 2:
+      arrows = 1
+   else:
+      arrows = 2
+elif l == 1:
+   if s == 1 / 2:
+      arrows = m + 2
+   else:
+      arrows = m + 5
+elif l == 2:
+   if s == 1 / 2:
+      arrows = m + 3
+   else:
+      arrows = m + 8
+elif l == 3:
+   if s == 1 / 2:
+      arrows = m + 4
+   else:
+      arrows = m + 11
+else:
+   print "Arguments 'l' & 's' are invalid"  
 
-        lvl = str(n) + spdf + str(arrows)
+lvl = str(n) + spdf + str(arrows)
 
-        def getElement(lvl):
-                return symdict[lvl]
+def getElement(lvl):
+        return symdict[lvl]
 
-        def getAtomicNum(sym):
-                return atnum[sym]
+def getAtomicNum(sym):
+        return atnum[sym]
 
-        print "\nThe element is: "
-        if getAtomicNum(getElement(lvl)) >= 100:
-                print "   " + getElement(lvl)
-        elif getAtomicNum(getElement(lvl)) < 10:
-                print " " + getElement(lvl)
-        else:
-                print "  " + getElement(lvl)
-        print getAtomicNum(getElement(lvl))
+print "\nThe element is: "
+if getAtomicNum(getElement(lvl)) >= 100:
+        print "   " + getElement(lvl)
+elif getAtomicNum(getElement(lvl)) < 10:
+        print " " + getElement(lvl)
+else:
+        print "  " + getElement(lvl)
+print getAtomicNum(getElement(lvl))
+
